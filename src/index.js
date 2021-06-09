364b0818729cf910d2ea1ed924cfd4d46e6f377e
Quan-p <quanp155@gmail.com>
2021-06-09 17:23:25 -0500

    Tested adding an event listener through JS to run a header changing function

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
+//mainDisplay = document.getElementById('mainDisplay');
+
 function clickInbox() {
-    
+    mainDisplay = document.getElementById('mainDisplay')
+    mainDisplay.innerHTML = 'new title';
 }
 
 function clickToday() {
@@ -9,3 +12,10 @@ function clickToday() {
 function clickWeek() {
 
 }
+
+function controlBtn() {
+var inboxBtn = document.getElementById('inboxBtn');
+inboxBtn.addEventListener('click', clickInbox);
+}
+
+controlBtn();
