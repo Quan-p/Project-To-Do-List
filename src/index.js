155ba8457a7b7c0be632749ad9bd7a1c3c524bbd
Quan-p <quanp155@gmail.com>
2021-07-07 15:45:37 -0500

    Removed button logic and moved to different module

--- src/index.js
+++ src/index.js
@@ -1,39 +1,15 @@
 import createGrid from './modules/inbox.js';
 import { openModal } from './modules/todo.js';
-
-function clickInbox() {
-    mainDisplay = document.getElementById('mainDisplay')
-    mainDisplay.innerHTML = 'Inbox';
-}
-
-function clickToday() {
-    mainDisplay = document.getElementById('mainDisplay')
-    mainDisplay.innerHTML = 'Today';
-}
-
-function clickWeek() {
-    mainDisplay = document.getElementById('mainDisplay')
-    mainDisplay.innerHTML = 'Week';
-}
-
-function controlBtn() {
-var inboxBtn = document.getElementById('inboxBtn');
-inboxBtn.addEventListener('click', clickInbox);
-
-var todayBtn = document.getElementById('todayBtn');
-todayBtn.addEventListener('click', clickToday);
-
-var weekBtn = document.getElementById('weekBtn');
-weekBtn.addEventListener('click', clickWeek);
+import btnControl from './modules/controller.js';
 
 createGrid();
-}
 
 function addItem() {
     var addProject = document.getElementById('addProject');
     addProject.addEventListener('click', openModal);
 }
 
+btnControl();
 addItem();
 controlBtn();
 
