f92b311f142ed58f2feaaf6b4cb328297184e3b6
Quan-p <quanp155@gmail.com>
2021-07-08 17:43:10 -0500

    Created add project function with event listener for clicking new project button

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
 import createGrid from './modules/inbox.js';
-import { openModal } from './modules/todo.js';
+import { openModal, openProject } from './modules/todo.js';
 import btnControl from './modules/controller.js';
 
 createGrid();
 
 function addItem() {
-    var addProject = document.getElementById('open-modal');
-    addProject.addEventListener('click', openModal);
+    var addTodo = document.getElementById('open-modal');
+    addTodo.addEventListener('click', openModal);
+}
+
+function addProject() {
+    var addProject = document.getElementById('addProject');
+    addProject.addEventListener('click', openProject);
 }
 
 btnControl();
 addItem();
+addProject();
 
