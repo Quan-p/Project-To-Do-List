3ad71742a28d0b5e2d29659d4bebfe2166e8cfec
Quan-p <quanp155@gmail.com>
2021-07-21 18:14:24 -0500

    Wrote function to create new task using DOM and exported function

--- src/modules/todo.js
+++ src/modules/todo.js
@@ -37,7 +37,14 @@ function formValue() {
     var priority = document.getElementById('priority').value;
     var dueDate = document.getElementById('due-date').value;
     var taskProject = document.getElementById('project').value;
-    console.log(taskDesc);
+    return {
+        taskTitle,
+        taskDesc,
+        priority,
+        dueDate,
+        taskProject
+    };
+    
 }
 
 // function validateForm() {
@@ -48,9 +55,29 @@ function formValue() {
 //     }
 // }
 
+function writeTask() {
+    var taskList = document.getElementById('taskList');
+    
+    var taskItem = document.createElement('li');
+    taskItem.textContent = taskTitle;
+    var check = document.createElement('input');
+    check.setAttribute('type', 'checkbox');
+    var delItem = document.createElement('span');
+    check.setAttribute('class', 'material-icons taskControl');
+    delItem.value = 'delete';
+    var edit = document.createElement('span');
+    check.setAttribute('class', 'material-icons taskControl');
+    edit.value = 'edit';
+    var date = document.createElement('div');
+    date.setAttribute('class', 'taskControl');
+    date.value = dueDate;
+
+}
+
 export {
     openModal,
     openProject,
-    formValue
+    formValue,
+    writeTask
     //validateForm
 }
